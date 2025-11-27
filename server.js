2cd871fbab34eda216089e2a77420c5ba126cc8d
Eva.Rodriguez <evarodriguez.24@campuscamara.es>
2025-11-27 12:41:19 +0100

    cambio

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+// Middleware para poder procesar datos en formato JSON
+app.use(express.json());
+// Definir una ruta básica (endpoint) de prueba
+app.get("/", (req, res) => {
+res.send("🚀 Servidor Express funcionando correctamente");
+});
+// Definir el puerto donde escuchará el servidor
+const PORT = 3000;
+// Iniciar el servidor y escuchar peticiones en el puerto definido
+app.listen(PORT, () => {
+console.log(`✅ Servidor en ejecución: http://localhost:${PORT}`);
+});
